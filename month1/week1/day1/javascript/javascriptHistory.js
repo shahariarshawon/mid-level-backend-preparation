0afededeea66932ccc56b4f907c95d0177d6f27b
shahariarshawon <shahariararafat7739@gmail.com>
2026-06-05 12:25:22 +0600

    Started the journey with JavaScript

---
+++ month1/week1/day1/javascript/javascriptHistory.js
@@ -0,0 +1,13 @@
+/**
+ * Javascript was made by Brendan Eich at 1995. First it was named "Mocha" then renamed to "LiveScript" then to get attention from people renamed again as "JavaScript". That time Java was a popilar language. People thought that maybe it is a new version of Java but actually it wasn't.
+ * After inventing this language, other companies were also making their own languages.
+ * It became difficult for the developer to handle all the things like which one they learn, which one they don't.
+ * Then an organization named "Echma Script" decided to make one global language to remove these confession and name it "JavaScript".
+ * As this language was made within 15 days there were several error. After update version of ES6 most of the problem has been solved.
+ */
+
+
+
+
+
+
