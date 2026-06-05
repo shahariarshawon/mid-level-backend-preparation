db7a18495100efd096190be41971330159e4a8cc
shahariarshawon <shahariararafat7739@gmail.com>
2026-06-05 22:03:14 +0600

    Variable

---
+++ month1/week1/day1/javascript/variable.js
@@ -0,0 +1,22 @@
+/**
+ *                              Keyword
+ * in programming language some words are fixed to use for some specific functionalities. examples: for, if, else, number, string etc
+ */
+
+// variables
+/**
+ * Variable means where we store the data.
+ * There are 3 types of variable exists.
+ * 1. var [we will never use it]
+ * 2. let
+ * 3. const
+ */
+// let value can be changed
+let value = "shawon";
+value = "rahman";
+console.log(value);
+
+// const value can not be changed
+const name = "Arafat";
+// name = "shawon";
+console.log(name);
