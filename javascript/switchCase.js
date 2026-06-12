f12728a641772db9ac99751d4b7ececc873a008c
shahariarshawon <shahariararafat7739@gmail.com>
2026-06-12 12:27:58 +0600

    Added switch case of javascript

---
+++ javascript/switchCase.js
@@ -0,0 +1,24 @@
+// this is called switch case which is used when we have to work base on  a single case handling
+
+
+let status = null
+switch (status) {
+
+    case "pending":
+        console.log("Status is pending")
+        break;
+
+    case "Submitted":
+        console.log("Satus is submitted");
+        break;
+
+    case "Failed":
+        console.log("Status is failed")
+        break
+
+    default:
+        console.log("Satus is unknown")
+
+
+}
+
