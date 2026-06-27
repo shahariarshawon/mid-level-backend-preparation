882e28fd67712cbd0396caef813feddd638eb3e2
shahariarshawon <shahariararafat7739@gmail.com>
2026-06-28 00:27:25 +0600

    Function

---
+++ javascript/function.js
@@ -0,0 +1,13 @@
+// normal funciton
+// function addition=(a,b){return a+b}
+const addition= function (a,b){
+    return a+b;
+}
+
+console.log(addition(3,4))
+
+// arrow function
+const subtraction=(a,b)=>{
+    return a-b;
+}
+console.log(subtraction(3,2))
